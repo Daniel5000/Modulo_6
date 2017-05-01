490725b38d55b495cf441d4d0303bb0180f1537d
dacasti5000@gmail.com <dacasti5000@gmail.com>
2017-04-30 22:57:07 -0500

    probando

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -1,6 +1,13 @@
 (function() {
 "use strict";
 
-angular.module('common', []);
+angular.module('common', [])
+.constant('ApiPath', 'https://daniel5.herokuapp.com')
+.config(config);
+
+config.$inject = ['$httpProvider'];
+function config($httpProvider) {
+  $httpProvider.interceptors.push('loadingHttpInterceptor');
+}
 
 })();
